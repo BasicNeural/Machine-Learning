079e609855c22b706296f676775c122abc4c2579
TenHexagon <wjsgmlcjs@gmail.com>
2017-01-02 08:06:08 +0900

    add file

---
+++ GD.hs
@@ -0,0 +1,19 @@
+module GD where
+
+import Data.Matrix
+
+gd' cost old new x y =
+    case increment > precision of 
+        True  -> gd' cost new (new - scaleMatrix eps (cost new x y)) x y
+        False -> new
+    where
+        precision = 0.0000000001
+        eps = 0.0001
+        toNum = sum . map abs . toList
+        increment = abs $ toNum new - toNum old
+
+f w x y = scaleMatrix len (multStd (multStd w x - y) $ transpose x)
+    where
+        len = fromIntegral (length . toList $ y) :: Double
+
+gd cost w x y = gd' cost w (w - scaleMatrix 0.0001 (cost w x y)) x y
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Machine-Learning
+이 프로젝트는 Haskell로 기계학습을 구현하기 위한 프로젝트 입니다.
