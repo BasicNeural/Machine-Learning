46471aebc03409a34d0b7cc8421565d20879c19c
TenHexagon <wjsgmlcjs@gmail.com>
2017-02-09 21:20:37 +0900

    hah!

--- Main.hs
+++ Main.hs
@@ -30,24 +30,17 @@ main = do
 
     let w = fromLists [replicate (nrows x) 0]
     
-    let result = trainMatrix 0.0001 20000 linear x y w
+    let result = trainMatrix 0.00001 100000 linear x y w
 
     putStrLn "학습 결과"
     putStrLn . show $ result
     
 {-
-키   무게  성공률 성공률(자유투) 평균득점
-7.4  240   0.599    0.713       17.1
-6.8  225   0.482    0.701       11.6
-6.8  215   0.457    0.734       5.8
-7    230   0.435    0.764       8.3
+128,46,167
+166,72,217
 -}
-    let d1 = M.transpose . fromLists $ [[1, 7.4, 2.40, 0.599, 0.713]]
-    let d2 = M.transpose . fromLists $ [[1, 6.8, 2.25, 0.482, 0.701]]
-    let d3 = M.transpose . fromLists $ [[1, 6.8, 2.15, 0.457, 0.734]]
-    let d4 = M.transpose . fromLists $ [[1, 7, 2.30, 0.435, 0.764]]
+    let d1 = M.transpose . fromLists $ [[1,128,46]]
+    let d2 = M.transpose . fromLists $ [[1,166,72]]
 
-    predict result d1 17.1
-    predict result d2 11.6
-    predict result d3 5.8
-    predict result d4 8.3
+    predict result d1 167
+    predict result d2 217
