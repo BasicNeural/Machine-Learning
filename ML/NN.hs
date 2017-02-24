a4d09ce75633972286e0c5b441be7f56830ed34a
TenHexagon <wjsgmlcjs@gmail.com>
2017-02-24 17:28:23 +0900

    역전파 작업중...

--- ML/NN.hs
+++ ML/NN.hs
@@ -28,12 +28,15 @@ sigmoid x = 1 / (1 + exp (-x))
 logistic :: Floating a => a -> a -> a->
 logistic y y' = - y * log y' - (1 - y) * log y'
 --미완성
-backpropagation rate network cost inputs outputs = map (\input, output) -> ) $ zip inputs outputs
+backpropagation rate network cost inputs outputs = 
     where
-        delta x y = deriveMatrix (cost y) $ executeLayer x network
-        updateNetwork x y = zipWith (\(Layer active synapseX) (Layer _ synapseY) -> Layer active (synapseX - synapseY) ) x y
+        hiddenLayer = reverse . init $ network
+        (Layer outActive outSynapse) = last network
+        deltay x y = deriveMatrix (cost y) y' * deriveMatrix outActive y'        --역전파 에서 가장 처음에 만들어지는 델타 함수
+            where y' = executeLayer network x
+        updateNetwork x y = zipWith (\(Layer active synapseX) (Layer _ synapseY) -> Layer active (synapseX - fmap (*rate) synapseY) ) x y    --시냅스의 미분 결과로 시냅스를 업데이트
         backpro [] _ _ = []
-        backpro ((Layer active synapse):layers) input delta = newDelta * h : backpro layers input newDelta
+        backpro ((Layer active synapse):layers) delta = newDelta * h : backpro layers newDelta
             where
-                h = executeLayer input $ reverse layers
-                newDelta = deriveMatrix (\x -> active . toScalar $ x) $ synapse * h
+                h = map (\x -> executeLayer x $ init . reverse $ layers)
+                newDelta = map (\x -> deriveMatrix (\wx -> fmap active wx) $ synapse * x) h
